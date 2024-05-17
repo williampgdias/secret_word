e9a3195926d9177295ea73aa53014847b1afeb1d
William Dias <smdwilliam89@gmail.com>
2024-05-17 16:50:47 -0300

    feat(components): Criando componentes do projeto.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,33 @@
-import StartScreen from '../components/StartScreen';
+'use client';
+
+// React
+import { userCallback, useEffect, useState } from 'react';
+
+// data
+import { wordsList } from '../data/words';
+
+// Components
+import StartScreen from '@/components/StartScreen';
+import Game from '@/components/Game';
+import GameOver from '@/components/GameOver';
+
+const stages = [
+    { id: 1, name: 'start' },
+    { id: 2, name: 'game' },
+    { id: 3, name: 'end' },
+];
 
 export default function Home() {
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList);
+
+    console.log(words);
+
     return (
         <div className="App">
-            <StartScreen />
+            {gameStage === 'start' && <StartScreen />}
+            {gameStage === 'game' && <Game />}
+            {gameStage === 'end' && <GameOver />}
         </div>
     );
 }
